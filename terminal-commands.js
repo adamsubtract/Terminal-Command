a8088c7d1e8b7a5f0f2fe2f673603fad184dfd67
Adam King <kingadam7@gmail.com>
2019-03-25 16:40:11 -0700

    Made code more scalable

--- terminal-commands.js
+++ terminal-commands.js
@@ -4,23 +4,23 @@ module.exports.ls = () => {
   fs.readdir('./', (err, files) => {
      const filesToString = files.reduce((acc, file) => {
       return `${acc} ${file} `;
-    }, '');
+    }, '\n');
 
     console.log(filesToString);
    });
 };
 
-module.exports.touch = () => {
-  fs.writeFile('afile.txt','My name is adam', (err) => {
+module.exports.touch = (fileName, fileContent) => {
+  fs.writeFile(fileName, fileContent, (err) => {
       if (err) {
           return `Error: ${err}`;
       }
-      console.log('File has been created');
+      console.log('File \'' + fileName + '\' has been created');
   })
 };
 
-module.exports.mkdir = () => {
-  fs.mkdir('afolder',(err) => {
+module.exports.mkdir = (afolder) => {
+  fs.mkdir(afolder,(err) => {
       if (err) {
           return `Error: ${err}`;
       }
